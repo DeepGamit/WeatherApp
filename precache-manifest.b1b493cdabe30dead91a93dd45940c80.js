832049296195eece187e0a90aa4196b7df562112
Deep Gamit <gamit.d@husky.neu.edu>
2020-08-26 04:14:53 -0400

    Updates

---
+++ precache-manifest.b1b493cdabe30dead91a93dd45940c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d992ed873c5d1af789a5e71b802b33a",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "b6c05ccb843a93189ecf",
+    "url": "/WeatherApp/static/css/main.ae045813.chunk.css"
+  },
+  {
+    "revision": "9b81585da9cc37527ec6",
+    "url": "/WeatherApp/static/js/2.acec7486.chunk.js"
+  },
+  {
+    "revision": "5002026598ed5d61cd3d68aaaa6f8e02",
+    "url": "/WeatherApp/static/js/2.acec7486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c05ccb843a93189ecf",
+    "url": "/WeatherApp/static/js/main.c6d934e1.chunk.js"
+  },
+  {
+    "revision": "752c6d0b6a1c82075c11",
+    "url": "/WeatherApp/static/js/runtime-main.433be469.js"
+  }
+]);
